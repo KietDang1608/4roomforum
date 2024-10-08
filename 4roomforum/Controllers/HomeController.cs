2ab77c4b88fbf4bde2e2fae62a9dcc0f20ba679e
Đặng Tuấn Kiệt <kietdang1608@gmail.com>
2024-10-08 15:23:01 +0700

    test getall data from services in mainpage

--- 4roomforum/Controllers/HomeController.cs
+++ 4roomforum/Controllers/HomeController.cs
@@ -1,7 +1,7 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using _4roomforum.Models;
-
+using System.Net.Http.Json;
 namespace _4roomforum.Controllers;
 
 public class HomeController : Controller
@@ -12,9 +12,47 @@ public class HomeController : Controller
     {
         _logger = logger;
     }
+    public void GetAllUser(){
+        using (var client = new HttpClient())
+        {
+            client.BaseAddress = new Uri("http://localhost:5043/");
+            var responseTask = client.GetAsync("api/user");
+            responseTask.Wait();
 
-    public IActionResult Index()
+            var result = responseTask.Result;
+            if (result.IsSuccessStatusCode)
+            {
+                var users = result.Content.ReadFromJsonAsync<IList<User>>().Result;
+                ViewBag.Users = users;
+            }
+            else
+            {
+                ViewBag.Users = new List<User>();
+                _logger.LogError("Server error. Please contact administrator.");
+            }
+        }
+    }
+    public async Task<IActionResult> Index()
     {
+        using (var client = new HttpClient())
+        {
+            client.BaseAddress = new Uri("http://localhost:5000/");
+            var responseTask = client.GetAsync("api/category");
+            responseTask.Wait();
+
+            var result = responseTask.Result;
+            if (result.IsSuccessStatusCode)
+            {
+                var categories = result.Content.ReadFromJsonAsync<IList<Category>>().Result;
+                ViewBag.Categories = categories;
+            }
+            else
+            {
+                ViewBag.Categories = new List<Category>();
+                _logger.LogError("Server error. Please contact administrator.");
+            }
+        }
+        GetAllUser();
         return View();
     }
 
