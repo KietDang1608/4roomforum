032840c109be9ec28bc71f52d8e40eadbabe7cf1
baypro089 <huynhhuy114@gmail.com>
2024-11-04 16:50:28 +0700

    add some files IReplyRepo, ReplyRepo  and exceptionhandlers
    add some files IReplyRepo, ReplyRepo  and DataExceptionhHandler

---
+++ MicroServices/PostService/ExceptionHandler/DataNotFoundException.cs
@@ -0,0 +1,14 @@
+﻿namespace PostService.ExceptionHandler
+{
+    public class DataNotFoundException : Exception
+    {
+        public DataNotFoundException() { }
+
+        public DataNotFoundException(string message)
+            : base(message) { }
+
+        public DataNotFoundException(string message, Exception inner)
+            : base(message, inner) { }
+    }
+
+}
