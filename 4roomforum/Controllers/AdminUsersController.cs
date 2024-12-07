05894e8932ccde7b688c548b5a0a16663b22a907
Đặng Tuấn Kiệt <kietdang1608@gmail.com>
2024-12-07 17:52:12 +0700

    add search user

--- 4roomforum/Controllers/AdminUsersController.cs
+++ 4roomforum/Controllers/AdminUsersController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using _4roomforum.DTOs;
 using _4roomforum.Models;
 using Microsoft.AspNetCore.Authorization;
 using _4roomforum.Services.Interfaces;
@@ -18,6 +19,7 @@ namespace _4roomforum.Controllers
         }
 
         // GET: AdminUsersController
+        [HttpGet]
         public async Task<ActionResult> Index()
         {
             var users = await _userService.GetAllUsers();
@@ -25,9 +27,42 @@ namespace _4roomforum.Controllers
 
             return View("~/Views/Admin/Users/Index.cshtml");
         }
-        
+        [HttpPost]
+        public async Task<IActionResult> Search(string searchData)
+        {
+            var users = await _userService.GetAllUsers();
+            var searchUsers = new List<UserDTO>();
+            foreach (var user in users)
+            {
+                try
+                {
+                    if (user.Email.Contains(searchData))
+                    {
+                        searchUsers.Add(user);
+                    }
+                    else if (user.UserName.Contains(searchData))
+                    {
+                        searchUsers.Add(user);
+                    }
+                    else if (user.UserId.ToString().Contains(searchData))
+                    {
+                        searchUsers.Add(user);
+                    }
+                    else if (user.RoleId.ToString().Contains(searchData))
+                    {
+                        searchUsers.Add(user);
+                    }
+                }
+                catch (ArgumentNullException ex)
+                {
+                    return RedirectToAction("Index");
+                }
 
-        
+            }
+            ViewBag.Users = searchUsers;
+            return View("~/Views/Admin/Users/Index.cshtml");
+        }
+
     }
     
 }
